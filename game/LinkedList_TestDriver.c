86e1e35e3286c25e1421b407cd1853db0e44dec1
Joshua S. Andrews <91918985+Heero1224@users.noreply.github.com>
2026-05-02 11:03:24 -0700

    LinkedList_Test_Driver INIT

--- game/LinkedList_TestDriver.c
+++ game/LinkedList_TestDriver.c
@@ -11,11 +11,26 @@ include "LinkedList.h"
 int main() {
 	/* create LL */
 	test_linked_list = linkedlist_init();
+	/* add 1, remove 1 */
+	printf("Test 1\n");
+	assert(test_linked_list->length==0);
+	linkedlist_add(test_linked_list, 1);
+	assert(test_linked_list->length==1);
+	assert(test_linked_list->head);
+	assert(test_linked_list->head->data==1);
+	linkedlist_pop(test_linked_list);
+	assert(test_linked_list->length==0);
+	assert(test_linked_list->head==NULL);
+	
 	/* add 20 things to test_linked_list */
+	printf("Test 2\n");
 	for (int i = 0; i < 21; i++) {
 		linkedlist_add(test_linked_list, i);
 	}
 	assert(test_linked_list->length==20);
-	linkedlist_print(test_linked_list);
+	curr = test_linked_list->head;
+	for (int i=0; i<21; i++) {
+		assert(curr->data==i);
+	}
 	
 }
