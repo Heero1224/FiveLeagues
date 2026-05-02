a03a19176ff5933815f812de4fb530ce28c7497f
Joshua S. Andrews <91918985+Heero1224@users.noreply.github.com>
2026-05-01 23:59:42 -0700

    Building Test Cases for LinkedList

--- game/LinkedList_TestDriver.c
+++ game/LinkedList_TestDriver.c
@@ -5,13 +5,17 @@ Purpose: COntains a testing suite for a custom LinkedList library
 */
 include <stdlib.h>
 include <stdio.h>
+include <assert.h>
 include "LinkedList.h"
 
 int main() {
 	/* create LL */
 	test_linked_list = linkedlist_init();
+	/* add 20 things to test_linked_list */
 	for (int i = 0; i < 21; i++) {
 		linkedlist_add(test_linked_list, i);
 	}
+	assert(test_linked_list->length==20);
 	linkedlist_print(test_linked_list);
 	
+}
