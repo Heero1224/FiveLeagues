b589b78848f97c757ee79aff7a50ba77c7e274e0
Joshua S. Andrews <91918985+Heero1224@users.noreply.github.com>
2026-05-12 16:50:31 -0700

    Hero_Generator.c INIT

---
+++ game/src/Hero_Generator.c
@@ -0,0 +1,11 @@
+/*
+Filename: Hero_Generator.c
+Author: Joshua S. Andrews
+Purpose: All functions relating to a Hero
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include "LinkedList.h"
+#include "Hero_Generator.h"
+
+Hero * hero_init() {
